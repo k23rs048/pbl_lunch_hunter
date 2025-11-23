eb1a90a607ad4d9b1e71a69eb4485a2273b682ab
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-11-24 08:43:35 +0900

    Create user_login.php

---
+++ src/user_login.php
@@ -0,0 +1,9 @@
+<from action="?do=usr_check" method="post">
+    <table>
+        <tr><td></td><td><input type="text" name="userid"></td></tr>
+        <tr><td></td><td></td></tr>
+        <tr><td></td><td>
+            <imput type="submit" value="送信">&nbsp;<input type="reset" vale="取消">
+        </td></tr>
+    </table>
+</from>
