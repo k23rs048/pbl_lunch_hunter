db55849dd71df9a746ad9f3829d1fc98ac961d1c
Rs047-el <209739223+Rs047-el@users.noreply.github.com>
2025-12-03 12:06:53 +0900

    Update Lunch_HunterDB_DATA.sql

--- docs/Lunch_HunterDB_DATA.sql
+++ docs/Lunch_HunterDB_DATA.sql
@@ -1,3 +1,10 @@
+INSERT INTO t_user
+ (user_id,user_l_name,user_f_name,user_l_kana,user_f_kana,user_account,password,usertype_id)
+VALUES
+ ('test','テスト','ユーザ','テスト','ユーザ','テスト','1234','1')
+,('admin','管理者','　','カンリシャ','　','管理者','5678','9')
+;
+
 INSERT INTO t_usertype
  (usertype_id,usertype)
 VALUES
